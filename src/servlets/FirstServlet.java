b1c59927f4d4c10e0cce29684b15c16571f2382f
DESKTOP-GKRDK5D\Artignan <guillaume.artignan@gmail.com>
2018-11-09 10:58:19 +0100

    Finalisation de la Servlet affichant bonjour avec le prénom

--- src/servlets/FirstServlet.java
+++ src/servlets/FirstServlet.java
@@ -10,6 +10,9 @@ public class FirstServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException {
 	  	
+		String[] tab = request.getRequestURL().toString().split("/");
+		String lastElement = tab[tab.length-1];
+		
 		response.setContentType( "text/html" );
 	    PrintWriter out = response.getWriter();
 	    out.println( "<HTML>" );
@@ -17,8 +20,7 @@ public class FirstServlet extends HttpServlet {
 	    out.println( "<TITLE>Page generee par une servlet</TITLE>" );
 	    out.println( "</HEAD>" );
 	    out.println( "<BODY>" );
-	    out.println( "<H1>Bonjour</H1>" );
-	    out.println( "<H2>"+request.getRequestURL().toString()+"</H2>" );
+	    out.println( "<H1>Bonjour "+lastElement+"</H1>" );
 	    out.println( "</BODY>" );
 	    out.println( "</HTML>" );
 	    out.close();
