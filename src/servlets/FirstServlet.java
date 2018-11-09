2394d87a32a79b88a905aec265084571c36b1fc4
DESKTOP-GKRDK5D\Artignan <guillaume.artignan@gmail.com>
2018-11-09 10:50:53 +0100

    Utilisation de l'implémentation HttpServlet implémentant une méthode doGET et doPOST

--- src/servlets/FirstServlet.java
+++ src/servlets/FirstServlet.java
@@ -2,40 +2,24 @@ package servlets;
 
 import java.io.*;
 import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class FirstServlet implements Servlet {
-  private ServletConfig cfg;
+public class FirstServlet extends HttpServlet {
 
-  public void init(ServletConfig config) throws ServletException {
-     cfg = config;
-     System.out.println("INITIALISATION");
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException {
+	  	response.setContentType( "text/html" );
+	    PrintWriter out = response.getWriter();
+	    out.println( "<HTML>" );
+	    out.println( "<HEAD>");
+	    out.println( "<TITLE>Page generee par une servlet</TITLE>" );
+	    out.println( "</HEAD>" );
+	    out.println( "<BODY>" );
+	    out.println( "<H1>Bonjour</H1>" );
+	    out.println( "</BODY>" );
+	    out.println( "</HTML>" );
+	    out.close();
   }
 
-  public ServletConfig getServletConfig() {
-    return cfg;
-  }
-
-  public String getServletInfo() {
-    return "Une servlet de test";
-  }	
-
-  public void destroy() {
-	  
-  }
-  
-
-  public void service (ServletRequest req,  ServletResponse res ) 
-  throws ServletException, IOException  {
-    res.setContentType( "text/html" );
-    PrintWriter out = res.getWriter();
-    out.println( "<HTML>" );
-    out.println( "<HEAD>");
-    out.println( "<TITLE>Page generee par une servlet</TITLE>" );
-    out.println( "</HEAD>" );
-    out.println( "<BODY>" );
-    out.println( "<H1>Bonjour</H1>" );
-    out.println( "</BODY>" );
-    out.println( "</HTML>" );
-    out.close();
-  }
 }
